6c26c03bebea81d271191f261677dc63902abd8a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-24 08:00:52 -0400

    add boilerplate for app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const https = require("https");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/inventoryDB", {useNewUrlParser: true});
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server listening on PORT 3000!");
+});
