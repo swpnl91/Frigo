1ac4fc462cd6c00a595424e72c37335c742a6033
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-25 12:12:05 -0400

    change name of DB

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/inventoryDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/frigoDB", {useNewUrlParser: true});
 
 
 // Creating schemas and collections for DB
